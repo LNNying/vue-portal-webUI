f928694867ebba23cfc161edf5594397334a7dce
liuningning <1754102225@qq.com>
2020-01-17 10:52:33 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+  	<github :paths="'https://github.com/HongqingCao/vue-portal-webUI'"></github>
+    <div class="content-wrapper">
+       <VpMenu :uniqueOpen="true" :router="true" activeName="index">
+          <VpSubmenu name="components">
+          <template slot="title">
+                                组件列表
+          </template>
+          <VpMenuItem :name="'index'">首页</VpMenuItem>
+          <VpMenuItem :name="'pi'">P1.新闻选项卡</VpMenuItem>
+          <VpMenuItem :name="'pii'">P2.平滑放大效果</VpMenuItem>
+          <VpMenuItem :name="'piii'">P3.轮播图</VpMenuItem>
+          <VpMenuItem :name="'piv'">P4.即时检索</VpMenuItem>
+          <VpMenuItem :name="'pv'">P5.点赞功能</VpMenuItem>
+          <VpMenuItem :name="'pvi'">P6.粒子动效</VpMenuItem>
+          <VpMenuItem :name="'pvii'">P7.循环滚动新闻</VpMenuItem>
+        </VpSubmenu>
+      </VpMenu>
+      <router-view class="router-view"/>
+    </div>
+    <backtop :height="100" :bottom="100" :right="40" :duration="1000">
+    </backtop>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<!--/*
+* @Introduce:          首页页面布局
+* @Author:             HongqingCao
+* @Date:               2018-07-25 17:37:22
+* @Last Modified by:   HongqingCao
+* @Last Modified time: 2018-07-27 12:29:31
+*/-->
+<template>
+	<div class="index-box">
+	   <div class="cover">
+			<div class="cover-main">
+				<p>
+					<img class="mediou-zoom-imgage" src="../../../static/logo.png" />
+				</p>
+				<h1><a class="anchor">vue-portal-webUI</a></h1>
+				<blockquote>
+	               <p><strong>基于Vue的门户网站UI组件</strong></p>
+				</blockquote>
+			    <p>
+			    	<a href="https://github.com/HongqingCao/vue-portal-webUI" target="_blank" class="github">GitHub</a>
+	                <a href="#/pi" class="getstart">Get Started</a>
+			    </p>
+			</div>
+	   </div>
+    </div>
+</template>
+<style lang="scss">
+ @import './index'
+</style>
